dfcee6dbce8dc4516cac23e22b7a80f4441b8844
Arpodol <arpodol@gmail.com>
2020-08-20 21:38:00 -0600

    Manually displaying routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+
+import DATA from './data.js'
 import './App.css';
+
 
 class App extends Component {
   render() {
+    const routeRows = DATA.routes.map((route, idx) => (
+      <tr key={idx}>
+        <td>{route.airline}</td>
+        <td>{route.src}</td>
+        <td>{route.dest}</td>
+      </tr>
+    ))
     return (
       <div className="app">
         <header className="header">
           <h1 className="title">Airline Routes</h1>
         </header>
         <section>
-          <p>
-            Welcome to the app!
-          </p>
+          <table className='routes-table'>
+            <thead>
+            <tr>
+              <th>airline</th>
+              <th>src</th>
+              <th>dest</th>
+            </tr>
+            </thead>
+            <tbody>
+            { routeRows }
+            </tbody>
+          </table>
         </section>
       </div>
     );
